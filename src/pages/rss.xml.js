c58ee4ddfa4c5582b249e1b09f2d2f4e81fbfb5f
Ruidy <ruidy.nemausat@gmail.com>
2025-01-01 21:56:06 +0100

    refactor: update the rss to use collections too

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,17 @@
 import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context) {
   return rss({
     title: "Astro Learner | Blog",
     description: "My journey learning Astro",
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    items: (await getCollection("blog")).map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/blog/${post.id}`,
+    })),
     customData: `<language>en-us</language>`,
   });
 }
