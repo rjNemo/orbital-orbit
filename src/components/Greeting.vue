959498b3dc6e369ca14aadb02893b134b6a2056d
Ruidy <ruidy.nemausat@gmail.com>
2025-01-01 21:08:47 +0100

    feat: add interactive astro island using vue

---
+++ src/components/Greeting.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { computed, ref } from "vue";
+
+const props = defineProps<{ messages: string[] }>();
+const greeting = ref(props.messages[0]);
+const randomMessage = () => {
+  greeting.value =
+    props.messages[Math.floor(Math.random() * props.messages.length)];
+};
+</script>
+
+<template>
+  <div>
+    <h3>{{ greeting }}! Thanks for stopping by!</h3>
+    <button @click="randomMessage">New greeting</button>
+  </div>
+</template>
